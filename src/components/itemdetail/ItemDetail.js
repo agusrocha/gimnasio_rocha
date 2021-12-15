8f40b863651450fb1988a5ec675ecef3d07ee859
Agus Rocha <rochagustina@outlook.com>
2021-12-15 19:43:20 -0300

    Revert "se guarda en un estado la cantidad de productos seleecionados"
    This reverts commit d47fce321a04021f358b7afeac2466d1db9fad3e.

--- src/components/itemdetail/ItemDetail.js
+++ src/components/itemdetail/ItemDetail.js
@@ -1,50 +1,30 @@
-import React, {useState} from 'react';
-import ItemCount from '../itemcount/ItemCount';
+import React from 'react';
 import './Itemdetail.css';
-import { Link } from 'react-router-dom';
 
 const ItemDetail = ({products})=>{
-
-    const [stock, setStock] = useState(15);
-    const [showCount, setShowCount] = useState(0);
-    
-    const onAdd = (count)=>{
-        setStock(stock - count);
-        
-        if (count <= stock)  {
-            console.log(`se agregaron ${count} y quedan ${stock - count} en stock`);
-            setShowCount(count);
-        }        
-    }
-        
-    console.log(showCount);
     return (
         products.length !== 0 ? (
             <div className='itemDetail'>
-                <picture className='itemDetailDiv'>
-                    <img src={products.img} alt={products.name} className='itemDetailImg'/>
-                </picture>
-                
-                <div className='itemDetailDescription'>    
-                    <h5 className='itemDetailProducto'>
-                        {products.name}
-                    </h5>
-                    <p className='itemDetailDescripcion'>
-                        {products.descripcion}
-                    </p>
-                    <p className='itemDetailProductoPrice'>
-                        {products.price}
-                    </p>
-                    <p> Stock: {products.stock}un.</p>
-                    { showCount === 0 ?
-                    <ItemCount onAdd={onAdd} initial={1} stock={15}/>
-                    :
-                    <Link to={'/cart'} className='carrito'> Ir al carrito </Link>}
-                </div>         
-            </div>  
-            ) : (
-                <div className='cargando'> Cargando...</div>
-            )
+            <picture className='itemDetailDiv'>
+                <img src={products.img} alt={products.name} className='itemDetailImg'/>
+            </picture>
+            
+            <div className='itemDetailDescription'>    
+                <h5 className='itemDetailProducto'>
+                    {products.name}
+                </h5>
+                <p className='itemDetailDescripcion'>
+                    {products.descripcion}
+                </p>
+                <p className='itemDetailProductoPrice'>
+                    {products.price}
+                </p>
+                <button> Agregar al carrito </button>
+            </div>         
+          </div>  
+        ) : (
+            <div className='cargando'> Cargando...</div>
+        )
     )
 }
 
